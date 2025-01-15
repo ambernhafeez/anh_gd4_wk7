2cf5199e8fda982e56c9a3f40303ea3c026cf7b4
Amber Hafeez <ambernoelle.h@gmail.com>
2025-01-15 11:54:04 +0000

    Day 2-3 updates

---
+++ Advanced Programming Concepts/Advanced Programming Concepts/Assets/Scripts/MainManager.cs
@@ -0,0 +1,78 @@
+using System.Net.NetworkInformation;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    // making this script into a singleton pattern class
+    // instance is the name we are giving the game object the script is attached to now
+    public static MainManager Instance;
+    public float timeElapsed;
+    public Color unitColour;
+
+    private void Awake()
+    // awake function is called before anything else
+    {
+        // making sure that there is always one of this and destroying any copies
+        if(Instance != null)
+        // if we already have a definition for instance...
+        {
+            Destroy(gameObject);
+        }
+
+        // setting the current object as Instance
+        // this refers to the current class, which is MainManager
+        Instance = this;
+
+        // making this object persistent
+        DontDestroyOnLoad(gameObject);
+
+        LoadColour();
+
+    }
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+    }
+
+    // tag as serialisable so that it can be converted to Json format by the JsonUtility class
+    [System.Serializable]
+    class SaveData
+    {
+        // color will be an array of three values - R, G, B
+        public Color _unitColour;
+
+    }
+
+    // serialisation, bridge between unity and JSON file
+    public void SaveColour()
+    {
+        // create new data from SaveData template defined above
+        SaveData data = new SaveData();
+        data._unitColour = unitColour;
+
+        // convert data to Json format
+        string jsonData = JsonUtility.ToJson(data);
+        // use a persistentDataPath so that game is always able to save to a path on diffent computers
+        File.WriteAllText(Application.persistentDataPath + "/saveFile.json", jsonData);
+        Debug.Log("New color saved: " + unitColour);
+    }
+
+    // reverse of SaveColour()
+    public void LoadColour()
+    {
+        string path = Application.persistentDataPath + "/saveFile.json";
+        if (File.Exists(path))
+        {
+            Debug.Log("json file exists");
+            string jsonData = File.ReadAllText(path);
+            // deserialisation using SaveData template
+            SaveData data = JsonUtility.FromJson<SaveData>(jsonData);
+
+            unitColour = data._unitColour;
+        }
+        
+    }
+
+}
---
+++ Advanced Programming Concepts/Advanced Programming Concepts/Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ProductivityUnit: Unit
+{
+    ResourcePile currentPile = null;
+    public float productivityMultiplier = 2;
+         
+    protected override void BuildingInRange()
+    {
+         if(currentPile == null)
+         {
+             // TRYING TO FIND A RESOURCE PILE
+             // m_Target is a protected Building, the parent class of the ResourcePile class
+             // casting a ResourcePile on m_Target to check if that object is a resource pile or not. If it is a ResourcePile, it will now be pile.
+             ResourcePile pile = m_Target as ResourcePile;
+             
+             // IF WE DO FIND A PLE, WE ARE MULTIPLYING THE PRODUCTION SPEED
+             if(pile != null)
+             {
+                 // settng the current pile to the one we found above
+                 currentPile = pile;
+                 // adding multiplier to the production speed
+                 currentPile.ProductionSpeed *= productivityMultiplier;
+             }
+         }
+    }
+    
+    // resets productivity speed when the unit leaves the resource pile 
+    void ResetProductivity()
+    {
+        if(currentPile != null)
+        {
+            currentPile.ProductionSpeed /= productivityMultiplier;
+            currentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity();
+        base.GoTo(target);   
+    }
+
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+}
+    
